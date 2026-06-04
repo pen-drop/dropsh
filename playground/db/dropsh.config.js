252eea8757f16ddecbe4d9589418ea3b466c4e23
Chrisitan Wiedemann <christian.wiedemann@key-tec.de>
2026-06-04 21:15:40 +0200

    chore(display-builder): register playground plugin

--- playground/db/dropsh.config.js
+++ playground/db/dropsh.config.js
@@ -1,8 +1,4 @@
-// Display Builder integration: subsite with basic_auth + display_builder + ui_patterns.
-// URL: http://db.dropsh-test.ddev.site
-//
-// The display-builder dropsh plugin is not yet implemented. This config is a
-// placeholder; once `plugins/display-builder` lands, add its import here.
+import { displayBuilderPlugin } from "@dropsh/plugin-display-builder";
 import { basicAuthPlugin } from "dropsh/plugin";
 
 export default {
@@ -12,6 +8,6 @@ export default {
   },
   plugins: [
     basicAuthPlugin({ username: "tester", password: "tester-pw" }),
-    // displayBuilderPlugin(),  // TODO once @dropsh/plugin-display-builder exists
+    displayBuilderPlugin(),
   ],
 };
